49f66c65877c5529a9d7809a009ff92eabc85da0
luukmiddelweerrd <luuk.middelweerd@live.nl>
2013-11-20 14:56:49 +0100

    mouseklik werkt

--- MazeRunner3/src/MainGame/UserInput.java
+++ MazeRunner3/src/MainGame/UserInput.java
@@ -59,32 +59,34 @@ public class UserInput extends Control
 	{
 	
 		// TODO: Set dX and dY to values corresponding to mouse movement
-		if(this.inscreen){
-			//only reset for next mouse-drag if mouse in-screen!
-			  this.dX = (xdragPos - xPos);
-			  this.dY = (ydragPos - yPos);
-			  this.xPos = xdragPos;
-			  this.yPos = ydragPos;	
-		}
-		else{
-			//Now we still rotate when the mouse has left the screen!
-				if((xdragPos - xPos) != 0 && (ydragPos - yPos) == 0){
-					this.dX = 1*(xdragPos - xPos)/(Math.abs(xdragPos - xPos));
-					this.dY = 0;
-				}
-				else if((xdragPos - xPos) == 0 && (ydragPos - yPos) != 0){
-					this.dX = 0;
-					this.dY = 1*(ydragPos - yPos)/(Math.abs(ydragPos - yPos));
-				}
-				else if((xdragPos - xPos) != 0 && (ydragPos - yPos) != 0){
-					this.dX = 1*(xdragPos - xPos)/(Math.abs(xdragPos - xPos));
-					this.dY = 1*(ydragPos - yPos)/(Math.abs(ydragPos - yPos));
-				}
-				else{
-					this.dX = 0;
-					this.dY = 0;
-				}
+		if (this.gsm.getCurState() == 1){
+			if(this.inscreen){
+				//only reset for next mouse-drag if mouse in-screen!
+				  this.dX = (xdragPos - xPos);
+				  this.dY = (ydragPos - yPos);
+				  this.xPos = xdragPos;
+				  this.yPos = ydragPos;	
 			}
+			else{
+				//Now we still rotate when the mouse has left the screen!
+					if((xdragPos - xPos) != 0 && (ydragPos - yPos) == 0){
+						this.dX = 1*(xdragPos - xPos)/(Math.abs(xdragPos - xPos));
+						this.dY = 0;
+					}
+					else if((xdragPos - xPos) == 0 && (ydragPos - yPos) != 0){
+						this.dX = 0;
+						this.dY = 1*(ydragPos - yPos)/(Math.abs(ydragPos - yPos));
+					}
+					else if((xdragPos - xPos) != 0 && (ydragPos - yPos) != 0){
+						this.dX = 1*(xdragPos - xPos)/(Math.abs(xdragPos - xPos));
+						this.dY = 1*(ydragPos - yPos)/(Math.abs(ydragPos - yPos));
+					}
+					else{
+						this.dX = 0;
+						this.dY = 0;
+					}
+				}
+		}
 	}
 
 	/*
@@ -97,37 +99,44 @@ public class UserInput extends Control
 	public void mousePressed(MouseEvent event)
 	{
 		// Detect the location where the mouse has been pressed
-    	this.xPos = event.getX();
-		this.yPos = event.getY();
-		xdragPos = xPos;
-		ydragPos = yPos;	
+		if (this.gsm.getCurState() == 1){
+	    	this.xPos = event.getX();
+			this.yPos = event.getY();
+			xdragPos = xPos;
+			ydragPos = yPos;	
+		}
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent event)
 	{
+		
 		// TODO: Detect mouse movement while the mouse button is down
-		this.xdragPos = event.getX();
-		this.ydragPos = event.getY();
+		if (this.gsm.getCurState() == 1){
+			this.xdragPos = event.getX();
+			this.ydragPos = event.getY();
+		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent event)
 	{
-
+		
 		// TODO: Set forward, back, left and right to corresponding key presses
-		if (event.getKeyChar() == 'w'){
-                  this.forward = true;
-                }
-                else if (event.getKeyChar() == 'a'){
-                  this.left = true;
-                }
-                else if (event.getKeyChar() == 's'){
-                  this.back = true;
-                }
-                else if (event.getKeyChar() == 'd'){
-                  this.right = true;
-                }
+		if (this.gsm.getCurState() == 1){
+			if (event.getKeyChar() == 'w'){
+              this.forward = true;
+            }
+            else if (event.getKeyChar() == 'a'){
+              this.left = true;
+            }
+            else if (event.getKeyChar() == 's'){
+              this.back = true;
+            }
+            else if (event.getKeyChar() == 'd'){
+              this.right = true;
+            }
+		}	
 	}
 
 	@Override
@@ -135,6 +144,7 @@ public class UserInput extends Control
 	{
 
 		// TODO: Set forward, back, left and right to corresponding key presses
+		if (this.gsm.getCurState() == 1){
                 if (event.getKeyChar() == 'w'){
                     this.forward = false;
                   }
@@ -147,6 +157,7 @@ public class UserInput extends Control
                   else if (event.getKeyChar() == 'd'){
                     this.right = false;
                   }
+		}       
 	}
 
 	/*
@@ -159,8 +170,10 @@ public class UserInput extends Control
 	public void mouseMoved(MouseEvent event)
 	{
 		//We wanna control without clicking!
-		this.xdragPos = event.getX();
-		this.ydragPos = event.getY();
+		if (this.gsm.getCurState() == 1){
+			this.xdragPos = event.getX();
+			this.ydragPos = event.getY();
+		}
 	}
 
 	@Override
@@ -171,6 +184,10 @@ public class UserInput extends Control
 	@Override
 	public void mouseClicked(MouseEvent event)
 	{
+		if(this.gsm.getCurState() == 0){
+			
+			System.out.println("klik");
+		}
 	}
 
 	@Override
@@ -185,8 +202,11 @@ public class UserInput extends Control
 	public void mouseExited(MouseEvent event)
 	{
 		this.inscreen = false;
-		this.xdragPos = event.getX();
-		this.ydragPos = event.getY();
+		
+		if (this.gsm.getCurState() == 1){
+			this.xdragPos = event.getX();
+			this.ydragPos = event.getY();
+		}
 	}
 
 	@Override
