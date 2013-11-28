b8566738cfdd3b7a53904838c74da68414d572e6
Jollyo <jornebo@planet.nl>
2013-11-28 13:44:44 +0100

    Editor fixed
    It somehow used the old draw methods, instead of those in the
    DrawingMenu Class

--- MazeRunner3/src/MainGame/MazeEditor.java
+++ MazeRunner3/src/MainGame/MazeEditor.java
@@ -46,7 +46,6 @@ public class MazeEditor implements GLEventListener {
 	private GLCanvas canvas;
 
 	private int screenWidth = 600, screenHeight = 600; // Screen size.
-	private float buttonSize = screenWidth / 11.0f < screenHeight / 10.f ? screenWidth / 11.0f : screenHeight / 10.f;
 	private float FOV = 45.0f;
 
 	private ArrayList<VisibleObject> visibleObjects; // A list of objects that will be displayed on screen.
@@ -313,7 +312,7 @@ public class MazeEditor implements GLEventListener {
 		gl.glLoadIdentity();
 		orthographicProjection(gl);
 		gl.glDisable(GL.GL_LIGHTING);
-		drawButtons(gl);
+		MenuDrawing.drawButtons(gl);
 		gl.glEnable(GL.GL_LIGHTING);
 		perspectiveProjection(gl, glu);
 		
@@ -387,123 +386,6 @@ public class MazeEditor implements GLEventListener {
 		camera.setVerAngle(editor.getVerAngle());
 
 		camera.calculateVRP();
-	}
-
-	/**
-	 * A method that draws the top left buttons on the screen.
-	 * 
-	 * @param gl
-	 */
-	private void drawButtons(GL gl) {
-		// Draw a puls on top of the first box
-		gl.glLineWidth(4);
-		gl.glColor3f(1.0f, 1.0f, 1.0f);
-		lineOnScreen(gl, buttonSize / 10.0f, screenHeight - buttonSize / 2.0f,
-				buttonSize * 9.0f / 10.0f, screenHeight - buttonSize / 2.0f);
-		lineOnScreen(gl, buttonSize / 2.0f, screenHeight - buttonSize / 10.0f,
-				buttonSize / 2.0f, screenHeight - buttonSize * 9.0f / 10.0f);
-
-		// Draw a minus on top of the second box.
-		lineOnScreen(gl, buttonSize * 1.10f, screenHeight - buttonSize / 2.0f,
-				buttonSize * 19.0f / 10.0f, screenHeight - buttonSize / 2.0f);
-
-		// Draw a cube on the fourth box.
-		boxOnScreen(gl, buttonSize * 3.10f, screenHeight - buttonSize* 0.9f, buttonSize * 0.8f);
-
-		// Draw a slope on the seventh box.
-		slopeOnScreen(gl, buttonSize * 6.10f, screenHeight - buttonSize * 0.9f, buttonSize * 0.8f, buttonSize * 0.8f);
-
-		// Draw a slope on the eighth box.
-		slopeOnScreen(gl, buttonSize * 7.10f, screenHeight - buttonSize * 0.9f, buttonSize * 0.8f, buttonSize * 0.4f);
-
-		//Draw a rectangle on the ninth box.
-		rectangleOnScreen(gl, buttonSize * 8.10f, screenHeight - buttonSize * 0.9f, buttonSize * 0.8f, buttonSize * 0.4f);
-
-		// Draw the background boxes
-		gl.glColor3f(0, 0.5f, 0f);
-		boxOnScreen(gl, 0.0f, screenHeight - buttonSize, buttonSize);
-
-		gl.glColor3f(0, 0, 0.5f);
-		boxOnScreen(gl, buttonSize, screenHeight - buttonSize, buttonSize);
-
-		gl.glColor3f(0.5f, 0, 0);
-		boxOnScreen(gl, 2*buttonSize, screenHeight - buttonSize, buttonSize);
-
-		gl.glColor3f(0.5f, 0.5f, 0);
-		boxOnScreen(gl, 3*buttonSize, screenHeight - buttonSize, buttonSize);
-
-		gl.glColor3f(0, 0.5f, 0f);
-		boxOnScreen(gl, 4*buttonSize, screenHeight - buttonSize, buttonSize);
-
-		gl.glColor3f(0, 0, 0.5f);
-		boxOnScreen(gl, 5*buttonSize, screenHeight - buttonSize, buttonSize);
-
-		gl.glColor3f(0.5f, 0, 0);
-		boxOnScreen(gl, 6*buttonSize, screenHeight - buttonSize, buttonSize);
-
-		gl.glColor3f(0.5f, 0.5f, 0);
-		boxOnScreen(gl, 7*buttonSize, screenHeight - buttonSize, buttonSize);
-
-		gl.glColor3f(0, 0.5f, 0f);
-		boxOnScreen(gl, 8*buttonSize, screenHeight - buttonSize, buttonSize);
-
-		gl.glColor3f(0, 0, 0.5f);
-		boxOnScreen(gl, 9*buttonSize, screenHeight - buttonSize, buttonSize);
-
-		gl.glColor3f(0.5f, 0, 0);
-		boxOnScreen(gl, 10*buttonSize, screenHeight - buttonSize, buttonSize);
-
-		// Draw line around buttons
-		gl.glColor3f(0.8f,  0.8f,  0.8f);
-		rectangleOnScreen(gl, 0, screenHeight - buttonSize * 1.1f, buttonSize * 11, buttonSize * 0.1f);
-		rectangleOnScreen(gl, buttonSize*11, screenHeight - buttonSize * 1.1f, buttonSize * 0.1f, buttonSize * 1.1f);
-	}
-
-	/**
-	 * Help method that uses GL calls to draw a line.
-	 */
-	private void lineOnScreen(GL gl, float x1, float y1, float x2, float y2) {
-		gl.glBegin(GL.GL_LINES);
-		gl.glVertex2f(x1, y1);
-		gl.glVertex2f(x2, y2);
-		gl.glEnd();
-	}
-
-	/**
-	 * Help method that uses GL calls to draw a square
-	 */
-	private void boxOnScreen(GL gl, float x, float y, float size)
-	{
-		gl.glBegin(GL.GL_QUADS);
-		gl.glVertex2f(x, y);
-		gl.glVertex2f(x + size, y);
-		gl.glVertex2f(x + size, y + size);
-		gl.glVertex2f(x, y + size);
-		gl.glEnd();
-	}
-	/**
-	 * Help method that uses GL calls to draw a slope
-	 */
-	private void slopeOnScreen(GL gl, float x, float y, float xSize, float ySize)
-	{
-		gl.glBegin(GL.GL_TRIANGLES);
-		gl.glVertex2f(x, y);
-		gl.glVertex2f(x + xSize, y);
-		gl.glVertex2f(x, y + ySize);
-		gl.glEnd();
-	}
-
-	/**
-	 * Help method that uses GL calls to draw a rectangle
-	 */
-	private void rectangleOnScreen(GL gl, float x, float y, float xSize, float ySize)
-	{
-		gl.glBegin(GL.GL_QUADS);
-		gl.glVertex2f(x, y);
-		gl.glVertex2f(x + xSize, y);
-		gl.glVertex2f(x + xSize, y + ySize);
-		gl.glVertex2f(x, y + ySize);
-		gl.glEnd();
 	}
 
 	/**
