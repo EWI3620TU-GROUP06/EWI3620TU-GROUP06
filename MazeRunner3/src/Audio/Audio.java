4122a51a2fa4d26535ed570bb791226168f33805
Tom-Hogervorst <t.a.hogervorst@student.tudelft.nl>
2013-11-28 10:06:56 +0100

    Added music package

--- MazeRunner3/src/Audio/Audio.java
+++ MazeRunner3/src/Audio/Audio.java
@@ -17,7 +17,7 @@ public class Audio {
 	public static void playMusic(String musicName) {
 		try{
 			stopMusic();
-			musicName = musicName + ".wav";
+			musicName = "src/Music/" + musicName + ".wav";
 			music = new BackgroundMusic(new File(musicName));	
 		}
 		catch(Exception e){
@@ -32,7 +32,7 @@ public class Audio {
 
 	public static void playSound(String soundName) {
 		try{
-			soundName = soundName + ".wav";
+			soundName = "src/Music/" + soundName + ".wav";
 			sound = new Sound(new File(soundName));	
 		}
 		catch(Exception e){
