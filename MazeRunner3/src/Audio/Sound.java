fbf07dfc90236723ad94cd22be27cad22d7907dd
Jollyo <jornebo@planet.nl>
2013-12-20 17:26:07 +0100

    Volume control

--- MazeRunner3/src/Audio/Sound.java
+++ MazeRunner3/src/Audio/Sound.java
@@ -5,12 +5,14 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 
 public class Sound implements Runnable{
 
 	private Thread soundThread;
 	private AudioListener listener = null;
 	private Clip clip = null;
+	private FloatControl volume;
 	
 	public Sound(File clipFile)
 	{
@@ -21,7 +23,7 @@ public class Sound implements Runnable{
 			clip = AudioSystem.getClip();
 			clip.addLineListener(listener);
 			clip.open(audioInputStream);
-
+			volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
 			audioInputStream.close();
 			
 			soundThread = new Thread(this);
@@ -30,7 +32,10 @@ public class Sound implements Runnable{
 		} catch(Exception e){
 			e.printStackTrace();
 		}
-
+	}
+	
+	public void setVolume(float gain){
+		volume.setValue(gain);
 	}
 
 	public void run()
