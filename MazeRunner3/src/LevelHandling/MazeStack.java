4fcc4c6e761bbecfd66e6ffec3d6e487970c2c21
Tom-Hogervorst <t.a.hogervorst@student.tudelft.nl>
2014-01-19 14:44:53 +0100

    Reading level fixed.

--- MazeRunner3/src/LevelHandling/MazeStack.java
+++ MazeRunner3/src/LevelHandling/MazeStack.java
@@ -16,7 +16,7 @@ public class MazeStack {
 
 	ArrayList<MazeObject> stack;
 	float mazeX, mazeZ;
-	
+
 	/**
 	 * Creates a new stack at the given location. the stack always has a bottom.
 	 * @param x	x coordinate of the stack
@@ -36,25 +36,28 @@ public class MazeStack {
 	 * drawn on the same place.
 	 * @param mzObj	The MazeObject to be added.
 	 */
-	
+
 	public void add(MazeObject mzObj)
 	{
-		if(size() == 1)
+		if(!(mzObj instanceof Pit || mzObj instanceof Bottom))
 		{
-			stack.add(Maze.standards.get(ObjectMode.ADD_PIT).translate(mazeX,  0, mazeZ));
-		}
-		float yMin = mzObj.getYMin();
-		float yMax = getTop().getYMax();
-		mzObj = mzObj.translate(0, yMax - yMin, 0);
-		
-		if( mzObj.hasBottom(stack.get(stack.size() - 1).getYMax()) || mzObj.getHeight() == 0 || mzObj instanceof FinishTile){
-			if(getTop().getHeight() < 0.1){
-				pop();
+			if(size() == 1)
+			{
+				stack.add(Maze.standards.get(ObjectMode.ADD_PIT).translate(mazeX,  0, mazeZ));
 			}
-			getTop().setTop(false);
+			float yMin = mzObj.getYMin();
+			float yMax = getTop().getYMax();
+			mzObj = mzObj.translate(0, yMax - yMin, 0);
+
+			if( mzObj.hasBottom(stack.get(stack.size() - 1).getYMax()) || mzObj.getHeight() == 0 || mzObj instanceof FinishTile){
+				if(getTop().getHeight() < 0.1){
+					pop();
+				}
+				getTop().setTop(false);
+			}
+
+			stack.add(mzObj);
 		}
-		
-		stack.add(mzObj);
 	}
 	/**
 	 * Removes the top element of the stack.
@@ -68,7 +71,7 @@ public class MazeStack {
 		}
 		getTop().setTop(true);
 	}
-	
+
 	/**
 	 * Gets all elements in the stack.
 	 * @return	The arraylist of Maze Objects in the stack
@@ -78,7 +81,7 @@ public class MazeStack {
 	{
 		return stack;
 	}
-	
+
 	/**
 	 * Returns the highest point in the stack.
 	 * @return	highest point of the top Object in the stack
@@ -88,7 +91,7 @@ public class MazeStack {
 	{
 		return stack.get(stack.size() - 1).getYMax();
 	}
-	
+
 	/**
 	 * Returns the number of Objects in the stack.
 	 * @return	Number of objects in the stack.
@@ -98,7 +101,7 @@ public class MazeStack {
 	{
 		return stack.size();
 	}
-	
+
 	public MazeObject getAtHeight(int height)
 	{
 		for(int i = 0; i < size(); i++)
@@ -110,7 +113,7 @@ public class MazeStack {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Gets the top Object in the stack.
 	 * @return	MazeObject at the top of the stack.
@@ -120,7 +123,7 @@ public class MazeStack {
 	{
 		return stack.get(stack.size() - 1);
 	}
-	
+
 	/**
 	 * Clears the stack.
 	 */
@@ -129,12 +132,12 @@ public class MazeStack {
 	{
 		stack.clear();
 	}
-	
+
 	/**
 	 * Removes one MazeObject of a certain kind from the stack.
 	 * @param mzObj	MazeObject type to be removed.
 	 */
-	
+
 	public void remove(MazeObject mzObj)
 	{
 		int index = stack.indexOf(mzObj);
@@ -143,7 +146,7 @@ public class MazeStack {
 			stack.get(index-1).setTop(true);
 		}
 	}
-	
+
 	/**
 	 * Gets the instance of a MazeObject in the stack that of the same kind as the given one.
 	 * @param that	Kind of MazeObject to be returned
@@ -165,7 +168,7 @@ public class MazeStack {
 		else
 			return null;
 	}
-	
+
 	/**
 	 * Rotates the top mazeObject by a certain angley Coordinate around which is rotated is calculated 
 	 * using the height of the top MazeObject
@@ -189,7 +192,7 @@ public class MazeStack {
 			stack.get(stack.size() - 2).setTop(!toBeRotated.hasBottom(stack.get(stack.size() - 2).getYMax()));
 		}
 	}
-	
+
 	/**
 	 * Rotates the top mazeObject by a certain angle
 	 * 
@@ -207,7 +210,7 @@ public class MazeStack {
 			stack.get(stack.size() - 2).setTop(!toBeRotated.hasBottom(stack.get(stack.size() - 2).getYMax()));
 		}
 	}
-	
+
 	/**
 	 * Rotates the top mazeObject by a certain angle. y Coordinate around which is rotated is calculated 
 	 * using the height of the top MazeObject.
@@ -231,14 +234,14 @@ public class MazeStack {
 			stack.get(stack.size() - 2).setTop(!toBeRotated.hasBottom(stack.get(stack.size() - 2).getYMax()));
 		}
 	}
-	
+
 	/**
 	 * Removes redundant faces that the given stack and this stack share. Also redundant faces inside this stack 
 	 * itself are removed.
 	 * 
 	 * @param that	MazeStack that is checked with this to remove all redundant faces. 
 	 */
-	
+
 	public void removeRedundantFaces(MazeStack that)
 	{
 		for(int i = 0; i < stack.size(); i++)
@@ -253,11 +256,11 @@ public class MazeStack {
 				{
 					thisObject.removeRedunantFaces(thatObject);
 				}
-					
+
 			}
 		}
 	}
-	
+
 	/**
 	 * Returns a standard stack that is used in an empty maze
 	 * @param x	x coordinate of the stack
@@ -268,7 +271,6 @@ public class MazeStack {
 	public static MazeStack standard(float x, float z)
 	{
 		MazeStack res = new MazeStack(x, z);
-		res.stack.add(Maze.standards.get(ObjectMode.ADD_PIT).translate(res.mazeX,  0, res.mazeZ));
 		res.stack.add(Maze.standards.get(ObjectMode.ADD_FLOOR).translate(res.mazeX,  0, res.mazeZ));
 		return res;
 	}
